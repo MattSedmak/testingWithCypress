a08c047ac9b470b9404ec873e8b215dfaab5751c
jesperkarlsson42 <jesper.karlsson@medieinstitutet.se>
2021-01-27 10:02:03 +0100

    added cypress test to login

--- cypress/integration/Login.js
+++ cypress/integration/Login.js
@@ -1,1 +1,23 @@
 // Skriva en test för login.
+
+describe("Login form", () => {
+    it("Can sign in", () => {
+        cy.visit("/Frontend/index.html");
+        cy.get("form");
+        cy.get("input[name='username']").type("CoolUser").should("have.value", "CoolUser");
+        cy.get("input[name='password']").type("123123123").should("have.value", "123123123");
+        cy.get("form").submit();
+        cy.url().should("include","start.html").end()
+    });
+
+    it("Can't sign in", () => {
+        cy.visit("/Frontend/index.html");
+        cy.get("form");
+        cy.get("input[name='username']").type("CoolUser");
+        cy.get("input[name='password']").type("1231231235");
+        cy.get("form").submit();
+        cy.contains("Fel användarnamn eller lösenord!").end()
+    })
+})
+
+
