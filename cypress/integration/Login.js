e4426885a19ac8769c9e1f3541e62e41bac49610
Arash <arash_raji@yahoo.com>
2021-01-27 19:10:44 +0100

    Tested the login functionality

--- cypress/integration/Login.js
+++ cypress/integration/Login.js
@@ -1,1 +1,9 @@
-// Skriva en test för login.
+describe('Testing Login Page', () => {
+    it('Should user be able to loggin', () => {
+        cy.visit("/Frontend/index.html")
+        cy.get("[name=username]").type("CoolUser")
+        cy.get("[name=password]").type("123123123")
+        cy.get("button[type=submit]").click()
+        cy.contains("Välkommen CoolUser")
+    })
+})
